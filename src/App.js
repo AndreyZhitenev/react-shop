1048c94f77e231a4c9c368bd28f6b0b979ba99b1
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-06-25 05:08:48 +0300

    added fetch for shop items, func add item to cart

--- src/App.js
+++ src/App.js
@@ -1,88 +1,31 @@
+import React from 'react';
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+
 function App() {
+	const [items, setItems] = React.useState([]);
+	const [cartItems, setCartItems] = React.useState([]);
+	const [cartOpened, setCartOpened] = React.useState(false);
+
+	React.useEffect(() => {
+		fetch('https://62b5e0e06999cce2e8fb9332.mockapi.io/items')
+			.then(res => {
+				return res.json();
+			})
+			.then(json => {
+				setItems(json);
+			});
+	}, []);
+
+	const onAddToCart = obj => {
+		setCartItems(prev => [...prev, obj]);
+	};
+
 	return (
 		<div className='wrapper clear'>
-			<div className='overlay'>
-				<div className='drawer'>
-					<h2 className='d-flex justify-between mb-30'>
-						Корзина
-						<img className='cu-p' src='/img/btn-remove.svg' alt='Remove' />
-					</h2>
-					<div className='items'>
-						<div className='cartItem d-flex align-center mb-20'>
-							<div
-								style={{
-									backgroundImage: 'url(/img/sneakers/1.jpg)',
-								}}
-								className='cartItemImg'></div>
-							<div className='mr-20 flex'>
-								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
-								<b>12 999 руб.</b>
-							</div>
-							<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
-						</div>
-						<div className='cartItem d-flex align-center mb-20'>
-							<div
-								style={{
-									backgroundImage: 'url(/img/sneakers/1.jpg)',
-								}}
-								className='cartItemImg'></div>
-							<div className='mr-20 flex'>
-								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
-								<b>12 999 руб.</b>
-							</div>
-							<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
-						</div>
-						<div className='cartItem d-flex align-center mb-20'>
-							<div
-								style={{
-									backgroundImage: 'url(/img/sneakers/1.jpg)',
-								}}
-								className='cartItemImg'></div>
-							<div className='mr-20 flex'>
-								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
-								<b>12 999 руб.</b>
-							</div>
-							<img className='removeBtn' src='/img/btn-remove.svg' alt='Remove' />
-						</div>
-					</div>
-					<div className='cartTotalBlock'>
-						<ul>
-							<li>
-								<span>Итого:</span>
-								<div></div>
-								<b>21 498 руб. </b>
-							</li>
-							<li>
-								<span>Налог 5%:</span>
-								<div></div>
-								<b>1074 руб. </b>
-							</li>
-						</ul>
-						<button className='greenButton'>
-							Оформить заказ <img src='/img/arrow.svg' alt='Arrow' />
-						</button>
-					</div>
-				</div>
-			</div>
-
-			<header className='d-flex justify-between align-center p-40'>
-				<div className='d-flex align-center'>
-					<img width={40} height={40} src='/img/logo.svg' alt='logo' />
-					<div className='headerInfo'>
-						<h3 className='text-uppercase'>React Shop</h3>
-						<p className='opacity-5'>Магазин на React</p>
-					</div>
-				</div>
-				<ul className='d-flex'>
-					<li className='mr-30'>
-						<img width={18} height={18} src='/img/cart.svg' alt='cart' />
-						<span>1205 руб.</span>
-					</li>
-					<li>
-						<img width={18} height={18} src='/img/user.svg' alt='user' />
-					</li>
-				</ul>
-			</header>
+			{cartOpened && <Drawer cartItems={cartItems} onClose={() => setCartOpened(false)} />}
+			<Header onClickCart={() => setCartOpened(true)} />
 			<div className='content p-40'>
 				<div className='d-flex align-center justify-between mb-40'>
 					<h1>Все товары</h1>
@@ -92,62 +35,17 @@ function App() {
 					</div>
 				</div>
 
-				<div className='d-flex'>
-					<div className='card'>
-						<div className='favorite'>
-							<img src='/img/heart-unliked.svg' alt='Unliked' />
-						</div>
-						<img width={133} height={112} src='/img/sneakers/1.jpg' alt='Sneakers' />
-						<h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-						<div className='d-flex justify-between align-center'>
-							<div className='d-flex flex-column'>
-								<span>Цена:</span>
-								<b>12 999 руб.</b>
-							</div>
-							<button className='button'>
-								<img width={11} height={11} src='/img/plus.svg' alt='plus' />
-							</button>
-						</div>
-					</div>
-					<div className='card'>
-						<img width={133} height={112} src='/img/sneakers/2.jpg' alt='Sneakers' />
-						<h5>Мужские Кроссовки Nike Air Max 270</h5>
-						<div className='d-flex justify-between align-center'>
-							<div className='d-flex flex-column'>
-								<span>Цена:</span>
-								<b>12 999 руб.</b>
-							</div>
-							<button className='button'>
-								<img width={11} height={11} src='/img/plus.svg' alt='plus' />
-							</button>
-						</div>
-					</div>
-					<div className='card'>
-						<img width={133} height={112} src='/img/sneakers/3.jpg' alt='Sneakers' />
-						<h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-						<div className='d-flex justify-between align-center'>
-							<div className='d-flex flex-column'>
-								<span>Цена:</span>
-								<b>8 499 руб.</b>
-							</div>
-							<button className='button'>
-								<img width={11} height={11} src='/img/plus.svg' alt='plus' />
-							</button>
-						</div>
-					</div>
-					<div className='card'>
-						<img width={133} height={112} src='/img/sneakers/4.jpg' alt='Sneakers' />
-						<h5>Кроссовки Puma X Aka Boku Future Rider</h5>
-						<div className='d-flex justify-between align-center'>
-							<div className='d-flex flex-column'>
-								<span>Цена:</span>
-								<b>8 999 руб.</b>
-							</div>
-							<button className='button'>
-								<img width={11} height={11} src='/img/plus.svg' alt='plus' />
-							</button>
-						</div>
-					</div>
+				<div className='d-flex flex-wrap'>
+					{items.map((item, index) => (
+						<Card
+							key={index}
+							title={item.title}
+							imageUrl={item.imageUrl}
+							price={item.price}
+							onFavorite={() => console.log('Добавили в закладки')}
+							onPlus={obj => onAddToCart(obj)}
+						/>
+					))}
 				</div>
 			</div>
 		</div>
